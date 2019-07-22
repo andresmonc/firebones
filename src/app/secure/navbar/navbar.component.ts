a193ad6bb3e58c2544d5531c7bead6d4fff17330
Jaime Moncayo <jaimeamonc@gmail.com>
2019-07-21 20:43:33 -0500

    added placeholder grid;  bootstrap style changes

--- src/app/secure/navbar/navbar.component.ts
+++ src/app/secure/navbar/navbar.component.ts
@@ -24,51 +24,50 @@ export class NavbarComponent implements OnInit {
 
   styleNav() {
     if (this.document.body.clientWidth < 399) {
-        this.bringTextCloser = true;
-        this.document.getElementById("navbar").style.padding = "0px 10px";
-        this.document.getElementById("navbar").style.height = "130px";
-        this.document.getElementById("navbar").style.top = "-2%";
-        this.document.getElementById("navbar").style.backgroundColor = "black";
-        this.document.getElementById("navbar").style.color = "#f9dfbe";
-        this.document.getElementById("logo").style.height = "auto";
-        this.document.getElementById("logo").style.transform = 'scale(' + (1 - 110 / 250) + ')'
-        this.document.getElementById("logo").style.webkitTransition = "all 2s ease";
-        this.document.getElementById("logo").style.transition = "all 2s ease";
-        this.document.getElementsByClassName("")
+      this.bringTextCloser = true;
+      this.document.getElementById("navbar").style.padding = "0px 10px";
+      this.document.getElementById("navbar").style.height = "130px";
+      this.document.getElementById("navbar").style.top = "-2%";
+      this.document.getElementById("navbar").style.backgroundColor = "black";
+      this.document.getElementById("navbar").style.color = "#f9dfbe";
+      this.document.getElementById("logo").style.height = "auto";
+      this.document.getElementById("logo").style.transform = 'scale(' + (1 - 110 / 250) + ')'
+      this.document.getElementById("logo").style.webkitTransition = "all 2s ease";
+      this.document.getElementById("logo").style.transition = "all 2s ease";
+      this.document.getElementsByClassName("")
     }
 
     else if (this.document.body.clientWidth < 900 && this.document.body.clientWidth > 400) {
-        this.bringTextCloser = true;
-        this.document.getElementById("navbar").style.padding = "0px 10px";
-        this.document.getElementById("navbar").style.height = "165px";
-        this.document.getElementById("navbar").style.top = "-2%";
-        this.document.getElementById("navbar").style.backgroundColor = "black";
-        this.document.getElementById("navbar").style.color = "#f9dfbe";
-        this.document.getElementById("logo").style.height = "auto";
-        this.document.getElementById("logo").style.transform = 'scale(' + (1 - 110 / 250) + ')'
-        this.document.getElementById("logo").style.webkitTransition = "all 2s ease";
-        this.document.getElementById("logo").style.transition = "all 2s ease";
+      this.bringTextCloser = true;
+      this.document.getElementById("navbar").style.padding = "0px 10px";
+      this.document.getElementById("navbar").style.height = "165px";
+      this.document.getElementById("navbar").style.top = "-2%";
+      this.document.getElementById("navbar").style.backgroundColor = "black";
+      this.document.getElementById("navbar").style.color = "#f9dfbe";
+      this.document.getElementById("logo").style.height = "auto";
+      this.document.getElementById("logo").style.transform = 'scale(' + (1 - 110 / 250) + ')'
+      this.document.getElementById("logo").style.webkitTransition = "all 2s ease";
+      this.document.getElementById("logo").style.transition = "all 2s ease";
     }
 
     else {
-        this.bringTextCloser = true;
-        this.document.getElementById("navbar").style.padding = "0px 10px";
-        this.document.getElementById("navbar").style.height = "265px";
-        this.document.getElementById("navbar").style.top = "-6%";
-        this.document.getElementById("navbar").style.backgroundColor = "black";
-        this.document.getElementById("navbar").style.color = "#f9dfbe";
-        this.document.getElementById("logo").style.height = "auto";
-        this.document.getElementById("logo").style.transform = 'scale(' + (1 - 110 / 250) + ')'
-        this.document.getElementById("logo").style.webkitTransition = "all 2s ease";
-        this.document.getElementById("logo").style.transition = "all 2s ease";
+      this.bringTextCloser = true;
+      this.document.getElementById("navbar").style.padding = "0px 10px";
+      this.document.getElementById("navbar").style.height = "265px";
+      this.document.getElementById("navbar").style.top = "-6%";
+      this.document.getElementById("navbar").style.backgroundColor = "black";
+      this.document.getElementById("navbar").style.color = "#f9dfbe";
+      this.document.getElementById("logo").style.height = "auto";
+      this.document.getElementById("logo").style.transform = 'scale(' + (1 - 110 / 250) + ')'
+      this.document.getElementById("logo").style.webkitTransition = "all 2s ease";
+      this.document.getElementById("logo").style.transition = "all 2s ease";
     }
   }
 
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.styleNav();
   }
-
 
 
 }

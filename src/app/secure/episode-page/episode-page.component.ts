445eb8c0f87ed9e4b012218b6f1e7948dc556091
Farhan-Islam <farhan.t.islam@gmail.com>
2019-10-06 15:37:44 -0700

    added dynamic video ids and slide in anmation

--- src/app/secure/episode-page/episode-page.component.ts
+++ src/app/secure/episode-page/episode-page.component.ts
@@ -15,6 +15,7 @@ export class EpisodePageComponent implements OnInit {
   public episodeDesc: string = this.episodeDetailsService.getEpisodeDesc(this.id);
   public episodeContent: JSON = this.episodeDetailsService.getEpisodeContentArray(this.id);
   public contentCount = localStorage.getItem('contentCount');
+  public episodeVideoId = '';
   public timelineEpisodeCount;
   player;
 
@@ -26,8 +27,41 @@ export class EpisodePageComponent implements OnInit {
     console.log('in Episode Page');
   }
 
+  ngOnInit() {
+    this.timelineEpisodeCount = this.getTimelineEpisodeCount();
+    const doc = (window as any).document;
+    const playerApiScript = doc.createElement('script');
+    playerApiScript.type = 'text/javascript';
+    playerApiScript.src = 'https://www.youtube.com/iframe_api';
+    doc.body.appendChild(playerApiScript);
+    (window as any).onYouTubeIframeAPIReady = () => {
+      this.player = new (window as any).YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        events: {
+          onReady: (event) => {
+            console.log('ARE WE READY FOR VID');
+            this.onPlayerReady(event);
+          }
+          ,
+          onStateChange: (event) => { this.onPlayerStateChange(event); }
+        },
+        playerVars: {
+          autoplay: 1,
+          controls: 1,
+          modestbranding: 1,
+          rel: 0,
+          showInfo: 0
+        }
+      });
+    };
+  }
 
   isEven(num) { return !(num % 2); }
+
+  setVideoId(epvideoId) {
+    this.player.loadVideoById(epvideoId);
+  }
 
   getTimelineEpisodeCount() {
     let timelineCount = 0;
@@ -44,63 +78,22 @@ export class EpisodePageComponent implements OnInit {
     return lastKeyCount;
   }
 
-  ngAfterViewInit() {
-    const doc = (window as any).document;
-    const playerApiScript = doc.createElement('script');
-    playerApiScript.type = 'text/javascript';
-    playerApiScript.src = 'https://www.youtube.com/iframe_api';
-    doc.body.appendChild(playerApiScript);
-  }
-
-
-  ngOnInit() {
-
-    this.timelineEpisodeCount = this.getTimelineEpisodeCount();
-    console.log(this.timelineEpisodeCount);
-
-    // seriously? can this be moved to it's own function????????????????????????
-    (window as any).onYouTubeIframeAPIReady = () => {
-      this.player = new (window as any).YT.Player('player', {
-        height: '500px',
-        width: '1000px',
-        videoId: 'HmAsUQEFYGI',
-        events: {
-          onReady: (event) => {
-            console.log('ARE WE READY FOR VID');
-            this.onPlayerReady(event);
-          }
-          ,
-          onStateChange: (event) => { this.onPlayerStateChange(event); }
-        },
-        playerVars: {
-          autoplay: 1,
-          controls: 1,
-          modestbranding: 1,
-          // playlist: 'UG3sfZKtCQI,ALZHF5UqnU4,x9ZkC3OgI78',
-          rel: 0,
-          showInfo: 0
-        }
-      });
-    };
-
-  }
-
-  // The API calls this function when the player's state changes.
-  onPlayerStateChange(event) {
-    console.log('Vid status number:', event.data);
-    if (event.data === 0) {
-      console.log('VIDEO HAS ENDED');
-      // if content count is less than 2
-      this.ddb.updateUserContentWatched();
+    // The API calls this function when the player's state changes.
+    onPlayerStateChange(event) {
+      console.log('Vid status number:', event.data);
+      if (event.data === 0) {
+        console.log('VIDEO HAS ENDED');
+        // if content count is less than 2
+        this.ddb.updateUserContentWatched();
+      }
     }
-  }
+    // The API will call this function when the video player is ready
+    onPlayerReady(event) {
+      console.log('what is this', event);
+      event.target.playVideo();
+    }
 
-  // The API will call this function when the video player is ready
-  onPlayerReady(event) {
-    console.log('what is this', event);
-    event.target.playVideo();
-  }
-
+  // tslint:disable-next-line:use-lifecycle-interface
 
 
 }

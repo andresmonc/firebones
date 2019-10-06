6ab8d7c206440c26d4cf3eec961a512dda8da3e2
Farhan-Islam <farhan.t.islam@gmail.com>
2019-10-05 23:30:16 -0700

    added circle line left right timeline styles

--- src/app/secure/episode-page/episode-page.component.ts
+++ src/app/secure/episode-page/episode-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DynamoDBService } from "../../service/ddb.service";
-import { EpisodeDetailsService } from "../../service/episode-details.service"
+import { DynamoDBService } from '../../service/ddb.service';
+import { EpisodeDetailsService } from '../../service/episode-details.service';
 
 @Component({
   selector: 'awscognito-angular2-app',
@@ -9,12 +9,12 @@ import { EpisodeDetailsService } from "../../service/episode-details.service"
   styleUrls: ['./episode-page.component.css']
 })
 export class EpisodePageComponent implements OnInit {
-  objectKeys = Object.keys
-  public id = this.route.snapshot.paramMap.get('id')
-  public episodeTitle: String = this.episodeDetailsService.getEpisodeTitle(this.id);
-  public episodeDesc: String = this.episodeDetailsService.getEpisodeDesc(this.id);
+  objectKeys = Object.keys;
+  public id = this.route.snapshot.paramMap.get('id');
+  public episodeTitle: string = this.episodeDetailsService.getEpisodeTitle(this.id);
+  public episodeDesc: string = this.episodeDetailsService.getEpisodeDesc(this.id);
   public episodeContent: JSON = this.episodeDetailsService.getEpisodeContentArray(this.id);
-  public contentCount = localStorage.getItem("contentCount");
+  public contentCount = localStorage.getItem('contentCount');
   public timelineEpisodeCount;
   player;
 
@@ -23,27 +23,29 @@ export class EpisodePageComponent implements OnInit {
     public ddb: DynamoDBService,
     public episodeDetailsService: EpisodeDetailsService
   ) {
-    console.log("in Episode Page");
+    console.log('in Episode Page');
   }
 
 
-  isEven(num) { return !(num % 2);}
+  isEven(num) { return !(num % 2); }
 
-  getTimelineEpisodeCount(){
+  getTimelineEpisodeCount() {
     let timelineCount = 0;
-    for (var key in this.episodeContent) {
+    const lastKeyCount = Object.keys(this.episodeContent).length;
+
+    // tslint:disable-next-line:forin
+    for (const key in this.episodeContent) {
+      console.log(timelineCount);
       timelineCount++;
-      if (key == this.contentCount){
+      if (key === this.contentCount) {
         return timelineCount;
       }
     }
+    return lastKeyCount;
   }
 
-
-
   ngAfterViewInit() {
-
-    const doc = (<any>window).document;
+    const doc = (window as any).document;
     const playerApiScript = doc.createElement('script');
     playerApiScript.type = 'text/javascript';
     playerApiScript.src = 'https://www.youtube.com/iframe_api';
@@ -52,21 +54,23 @@ export class EpisodePageComponent implements OnInit {
 
 
   ngOnInit() {
-    this.timelineEpisodeCount = this.getTimelineEpisodeCount();
 
-    //seriously? can this be moved to it's own function????????????????????????
-    (<any>window).onYouTubeIframeAPIReady = () => {
-      this.player = new (<any>window).YT.Player('player', {
+    this.timelineEpisodeCount = this.getTimelineEpisodeCount();
+    console.log(this.timelineEpisodeCount);
+
+    // seriously? can this be moved to it's own function????????????????????????
+    (window as any).onYouTubeIframeAPIReady = () => {
+      this.player = new (window as any).YT.Player('player', {
         height: '500px',
         width: '1000px',
         videoId: 'HmAsUQEFYGI',
         events: {
-          'onReady': (event) => {
-            console.log("ARE WE READY FOR VID")
+          onReady: (event) => {
+            console.log('ARE WE READY FOR VID');
             this.onPlayerReady(event);
           }
           ,
-          'onStateChange': (event) => { this.onPlayerStateChange(event); }
+          onStateChange: (event) => { this.onPlayerStateChange(event); }
         },
         playerVars: {
           autoplay: 1,
@@ -83,20 +87,20 @@ export class EpisodePageComponent implements OnInit {
 
   // The API calls this function when the player's state changes.
   onPlayerStateChange(event) {
-    console.log("Vid status number:", event.data);
-    if (event.data == 0) {
-      console.log('VIDEO HAS ENDED')
-      //if content count is less than 2
+    console.log('Vid status number:', event.data);
+    if (event.data === 0) {
+      console.log('VIDEO HAS ENDED');
+      // if content count is less than 2
       this.ddb.updateUserContentWatched();
     }
   }
 
   // The API will call this function when the video player is ready
   onPlayerReady(event) {
-    console.log("what is this", event);
+    console.log('what is this', event);
     event.target.playVideo();
   }
-   
+
 
 
 }

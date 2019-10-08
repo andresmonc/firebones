bc1809da560b0f06759d4199b04e731769012632
Jaime Moncayo <jaimeandresmonc@gmail.com>
2019-10-07 19:32:29 -0500

    notifications button calls dummy function

--- src/app/secure/profile/myprofile.component.ts
+++ src/app/secure/profile/myprofile.component.ts
@@ -1,20 +1,20 @@
-import {Component} from "@angular/core";
-import {UserLoginService} from "../../service/user-login.service";
-import {Callback, CognitoUtil, LoggedInCallback} from "../../service/cognito.service";
-import {UserParametersService} from "../../service/user-parameters.service";
-import {Router} from "@angular/router";
-import {Location} from '@angular/common';
+import { Component } from "@angular/core";
+import { UserLoginService } from "../../service/user-login.service";
+import { Callback, CognitoUtil, LoggedInCallback } from "../../service/cognito.service";
+import { UserParametersService } from "../../service/user-parameters.service";
+import { Router } from "@angular/router";
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'awscognito-angular2-app',
     templateUrl: './myprofile.html',
-    styleUrls: ['./myprofile.css','./toggleswitch.css']
+    styleUrls: ['./myprofile.css', './toggleswitch.css']
 })
 export class MyProfileComponent implements LoggedInCallback {
 
     public parameters: Array<Parameters> = [];
     public cognitoId: String;
-    
+
 
     constructor(
         public router: Router,
@@ -22,7 +22,7 @@ export class MyProfileComponent implements LoggedInCallback {
         public userParams: UserParametersService,
         public cognitoUtil: CognitoUtil,
         private _location: Location
-        ) {
+    ) {
         this.userService.isAuthenticated(this);
     }
     isLoggedIn(message: string, isLoggedIn: boolean) {
@@ -35,7 +35,17 @@ export class MyProfileComponent implements LoggedInCallback {
 
     backClicked() {
         this._location.back();
-      }
+    }
+
+    updatePhoneNumber() {
+        console.log('I WAS UPDATED!!!!!!')
+    }
+
+    updateNotifications(){
+        console.log('WHY WOULDN\'T YOU WANT NOTIFCATIONS')
+    }
+
+
 }
 
 export class Parameters {
@@ -66,4 +76,5 @@ export class GetParametersCallback implements Callback {
         param.value = this.cognitoUtil.getCognitoIdentity();
         this.me.parameters.push(param)
     }
+
 }

65bfa99300d748eb7aef3f12ace15d041a987d05
Jaime Moncayo <jaimeandresmonc@gmail.com>
2019-10-09 21:40:46 -0500

    switched from cognitoparams to local storage

--- src/app/secure/profile/myprofile.component.ts
+++ src/app/secure/profile/myprofile.component.ts
@@ -1,11 +1,10 @@
-import { Component } from "@angular/core";
-import { UserLoginService } from "../../service/user-login.service";
-import { Callback, CognitoUtil, LoggedInCallback } from "../../service/cognito.service";
-import { UserParametersService } from "../../service/user-parameters.service";
-import { Router } from "@angular/router";
+import { Component } from '@angular/core';
+import { UserLoginService } from '../../service/user-login.service';
+import { Callback, CognitoUtil, LoggedInCallback } from '../../service/cognito.service';
+import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-import { Inject } from "@angular/core";
-import { DOCUMENT } from "@angular/common";
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { DynamoDBService } from '../../service/ddb.service';
 
 
@@ -16,32 +15,26 @@ import { DynamoDBService } from '../../service/ddb.service';
 })
 export class MyProfileComponent implements LoggedInCallback {
 
-    public parameters: Array<Parameters> = [];
     public cognitoId: string;
     public errorMessage: string;
     public phoneInput = '';
     public editMode: boolean;
+
     public notifications: boolean = localStorage.getItem('notifications') === 'true';
-
     public phoneNumber = localStorage.getItem('phoneNumber');
-
+    public email: string = localStorage.getItem('email');
 
     constructor(
         public router: Router,
         public userService: UserLoginService,
-        public userParams: UserParametersService,
-        public cognitoUtil: CognitoUtil,
         private location: Location,
         public ddb: DynamoDBService,
-        @Inject(DOCUMENT) private document: Document
     ) {
         this.userService.isAuthenticated(this);
     }
     isLoggedIn(message: string, isLoggedIn: boolean) {
         if (!isLoggedIn) {
             this.router.navigate(['/home/login']);
-        } else {
-            this.userParams.getParameters(new GetParametersCallback(this, this.cognitoUtil));
         }
     }
 
@@ -85,7 +78,7 @@ export class MyProfileComponent implements LoggedInCallback {
                     this.errorMessage = 'Please use only numbers in this format +015555555555'
                     console.log(currentDigit);
                     return false;
-                };
+                }
                 i++;
             }
             if (phoneNumber.charAt(0) !== '+') {
@@ -95,42 +88,11 @@ export class MyProfileComponent implements LoggedInCallback {
                 return true;
             }
         } catch {
-            this.errorMessage = 'Please enter a phone number.'
+            this.errorMessage = 'Please enter a phone number.';
             return false;
         }
 
     }
 
-
-}
-
-export class Parameters {
-    name: string;
-    value: string;
-}
-
-export class GetParametersCallback implements Callback {
-
-    constructor(public me: MyProfileComponent, public cognitoUtil: CognitoUtil) {
-
-    }
-
-    callback() {
-
-    }
-
-    callbackWithParam(result: any) {
-
-        for (let i = 0; i < result.length; i++) {
-            let parameter = new Parameters();
-            parameter.name = result[i].getName();
-            parameter.value = result[i].getValue();
-            this.me.parameters.push(parameter);
-        }
-        let param = new Parameters()
-        param.name = "cognito ID";
-        param.value = this.cognitoUtil.getCognitoIdentity();
-        this.me.parameters.push(param)
-    }
 
 }

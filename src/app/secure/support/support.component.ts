3dfb1e0fab3c5f9a11169151303514fd11fa2ee8
Farhan-Islam <farhan.t.islam@gmail.com>
2019-11-27 16:16:52 -0600

    finished support section

--- src/app/secure/support/support.component.ts
+++ src/app/secure/support/support.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EmailService } from '../../service/email.service';
 import { EmailReturn } from '../../models/emailResponse';
-
-
+import {MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { GlobalMessageModalComponent } from '../../global-message-modal/global-message-modal.component';
 
 @Component({
   selector: 'app-support',
@@ -16,7 +16,22 @@ export class SupportComponent implements OnInit {
   public response: EmailReturn;
   public supportEmail: 'jaimeamonc@gmail.com';
 
-  constructor(private emailService: EmailService) { }
+  constructor(private emailService: EmailService, private dialog: MatDialog) { }
+
+  openDialog(headerText: string, text: string, buttonText: string) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    dialogConfig.data = {
+        modalHeader: headerText,
+        modalText: text,
+        modalButtonText: buttonText
+    };
+
+    this.dialog.open(GlobalMessageModalComponent, dialogConfig);
+}
 
   ngOnInit() {
 
@@ -25,6 +40,13 @@ export class SupportComponent implements OnInit {
   submit() {
     this.emailService.postContact(this.supportEmail, this.supportTextData).subscribe((res) => {
       this.response = res;
+      if (res.statusCode === 200) {
+        this.openDialog('', 'Support request sucessfully sent!', 'Close');
+
+        this.supportTextData = '';
+      } else {
+        this.openDialog('', 'An Error occured', 'Close');
+      }
       console.log(res);
     });
   }
